2f8aab3521b862d22ca1831e00ebea5ff3097f7c
spline <spline@spline.com>
2012-07-09 18:01:51 -0400

    Initial push

---
+++ test.py
@@ -0,0 +1,14 @@
+###
+# Copyright (c) 2012, spline
+# All rights reserved.
+#
+#
+###
+
+from supybot.test import *
+
+class OlympicsTestCase(PluginTestCase):
+    plugins = ('Olympics',)
+
+
+# vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
